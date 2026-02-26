0c0ff0bea3d94cd65dee056e7674e20ae530294c
nicolaspadovam <nicolaspadovam@hotmail.com>
2026-02-25 23:48:00 -0300

    feat: adicionando o modo portrait e landscape

--- app/src/main/java/br/com/nicolaspadovam/pokemonstarters/MainActivity.kt
+++ app/src/main/java/br/com/nicolaspadovam/pokemonstarters/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.nicolaspadovam.pokemonstarters
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,12 +9,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -57,21 +63,93 @@ val starters = listOf(
 fun PokemonStarterScreen(modifier: Modifier = Modifier) {
     var pokemonSelected by remember { mutableStateOf(starters.first()) }
 
+    val config = LocalConfiguration.current
+    val isPortrait = config.orientation == Configuration.ORIENTATION_PORTRAIT
+
+    if (isPortrait) {
+        PokemonStarterScreenPortrait(
+            modifier = modifier,
+            pokemonSelected = pokemonSelected,
+            onSelected = { pokemonSelected = it }
+        )
+    } else {
+        PokemonStarterScreenLandscape(
+            modifier = modifier,
+            pokemonSelected = pokemonSelected,
+            onSelected = { pokemonSelected = it }
+        )
+    }
+
+
+}
+
+@Composable
+fun PokemonStarterScreenPortrait(
+    modifier: Modifier = Modifier,
+    pokemonSelected: Pokemon,
+    onSelected: (Pokemon) -> Unit
+) {
+
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        PokemonHeader("Escolha seu Pokémon inicial")
+        PokemonHeader("Escolha seu Pokémon Inicial")
+        Spacer(modifier = Modifier.weight(1f))
         PokeCard(pokemonSelected)
-
-        PokeOptionList(pokemons = starters, pokemonSelected = pokemonSelected) {
-            pokemonSelected = it
-        }
+        Spacer(modifier = Modifier.weight(1f))
+        PokeOptionList(
+            pokemons = starters,
+            pokemonSelected = pokemonSelected,
+            onSelected = onSelected
+        )
     }
 }
+
+@Composable
+fun PokemonStarterScreenLandscape(
+    modifier: Modifier = Modifier,
+    pokemonSelected: Pokemon,
+    onSelected: (Pokemon) -> Unit
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        //Esquerda -> Pokemon selecionado
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            PokeCard(pokemon = pokemonSelected)
+        }
+        //Direita -> opcoes de pokemon
+        Box(
+            modifier = modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                PokemonHeader("Escolha seu Pokemon inicial")
+                Spacer(modifier = Modifier.height(32.dp))
+                PokeOptionList(
+                    pokemons = starters,
+                    pokemonSelected = pokemonSelected,
+                    onSelected = onSelected
+                )
+            }
+        }
+    }
+
+
+}
+
 
 
 @Composable
